94b144de1ed55878ea55cfb15930c5606915a002
Malte Schink <13069402+m4schini@users.noreply.github.com>
2022-11-20 16:01:21 +0100

    more logs

--- main.go
+++ main.go
@@ -13,12 +13,17 @@ func main() {
 		port = "8080"
 	}
 
+	log.Println("started. listening on: " + port)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("=> NEW REQUEST! <=")
 		fmt.Println("referer:", r.Referer())
 		fmt.Println("=> <=")
 
-		w.Write([]byte(`<head><title>malte</title></head><body>moin</body>`))
+		_, err := w.Write([]byte(`<head><title>malte</title></head><body>moin</body>`))
+		if err != nil {
+			log.Fatalln(err)
+		}
 	})
 	if err := http.ListenAndServe("localhost:"+port, nil); err != nil {
 		log.Fatalln(err)
